560f29e35f0d5a2e75349d764e6a4810d4f5d346
lauraknorr <laura.knorr@campus.tu-berlin.de>
2019-05-10 14:25:35 +0200

    merge conflict

--- Aufgabe2/todos/todoapp/views.py
+++ Aufgabe2/todos/todoapp/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render, HttpResponse
+form .forms import TodoForm
 
 # Create your views here.
 def home(request):
     return render(request, 'todotemps/uebersicht.html')
 
 def neuesTodo(request):
+
+if request.method == 'POST'
+    form = TodoForm(request.POST)
+    if form.is_valid():
+        titel = form.cleaned_data['titel']
+        beschreibung = form.cleaned_data['Beschreibung']
+        tag = form.cleaned_data['tag']
+        monat = form.cleaned_data['monat']
+
+
+
+    form = TodoForm()
     return render(request, 'todotemps/neuesTodo.html')
 
 def bearbeitungTodo(request):
