f457a50b4f9f4091b8acfd45c1906215c90a9104
Emina Hrustic <amira.hrustic@gmx.de>
2019-05-11 13:44:06 +0200

    neuesTodo

--- Aufgabe2/todos/todoapp/views.py
+++ Aufgabe2/todos/todoapp/views.py
@@ -10,16 +10,6 @@ def home(request):
     return render(request, 'todotemps/uebersicht.html', context)
 
 def neuesTodo(request):
-
-    if request.method == 'POST':
-        form = TodoForm(request.POST)
-        if form.is_valid():
-            titel = form.cleaned_data['titel']
-            beschreibung = form.cleaned_data['Beschreibung']
-            tag = form.cleaned_data['tag']
-            monat = form.cleaned_data['monat']
-
-    form = TodoForm()
     return render(request, 'todotemps/neuesTodo.html')
 
 def bearbeitungTodo(request):
